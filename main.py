4c8584eff96f01b9b4ffe72749621581ad8158f8
wangzk <wangzk@nuaa.edu.cn>
2018-05-15 15:05:39 +0800

    add the sum of a & b

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("life is short,you need python!")
+#!/usr/bin/python3
+#coding=utf-8
+
+print ("Life is short, you need Python !")
+
+a = 10
+b = 20
+c = a + b
+print("The value of c is  ", c)
